093f010157162340f01fae8addac3ede4309df08
Aman Kumar <amankumaroo784@gmail.com>
2024-10-04 10:04:30 +0530

    server and express-validator setup

--- backend/index.ts
+++ backend/index.ts
@@ -1,23 +1,20 @@
-import express from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.ts";
-// import authRoutes from "./routes/auth.routes.ts";
+import authRoutes from "./routes/auth.routes.ts";
 import { json } from "express";
 
-// Load environment variables
 dotenv.config();
 
-// Connect to MongoDB
-connectDB();
+const app: Express = express();
 
-const app = express();
 app.use(json());
 
-// Routes
-app.use("/api/auth", () => {});
+app.use("/api/auth", authRoutes);
 
-// Start server
 const PORT = process.env.PORT || 5001;
+
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
+  connectDB();
 });
