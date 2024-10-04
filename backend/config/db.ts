9e6a057dfca3aa28b2e25a100685b7b6c286e17d
Aman Kumar <amankumaroo784@gmail.com>
2024-10-04 10:20:51 +0530

    added mongo db connections

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,1 +1,14 @@
-export default function connectDB() {}
+import mongoose from "mongoose";
+
+const connectDB = async (): Promise<void> => {
+  const uri = process.env.MONGO_URI as string;
+  try {
+    await mongoose.connect(uri);
+    console.log("database connected");
+  } catch (error: any) {
+    console.error(`error connecting database: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
